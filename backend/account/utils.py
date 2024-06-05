c0bf067c569f9dc77e45b6f2445df30f2df576c3
user <kola89611@gmail.com>
2024-06-05 19:27:53 +0300

    Added: base views for account

---
+++ backend/account/utils.py
@@ -0,0 +1,12 @@
+from account.models import UserProfile
+
+
+def user_additional_models(request):
+    profile_user = UserProfile.objects.filter(user=request.user).exists()
+
+    if not profile_user:
+        profile_user = UserProfile()
+        profile_user.user = request.user
+        profile_user.save()
+
+    return 'Models Created'
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib import messages
 
-# Create your views here.
+from .forms import UserRegisterForm, UserLoginFrom
+from .utils import user_additional_models
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                user = form.save()
+                login(request, user)
+
+                user_additional_models(request)
+
+                messages.success(request, 'You successfully registered!')
+                return redirect('home')
+            else:
+                messages.error(request, 'Registration error.')
+        else:
+            form = UserRegisterForm()
+        return render(request, 'user/register.html', {'form': form})
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            form = UserLoginFrom(data=request.POST)
+            if form.is_valid():
+                user = form.get_user()
+                login(request, user)
+
+                user_additional_models(request)
+
+                messages.success(request, f'Welcome back {str(request.user.username).title()}. You successfully logged in!')
+                return redirect('home')
+            else:
+                for error in list(form.errors.values()):
+                    messages.error(request, error)
+        else:
+            form = UserLoginFrom()
+        return render(request, 'user/login.html', {'form': form})
+
+
+def logout(request):
+    logout(request)
+    return redirect('login')
