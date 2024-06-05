a17a4e62c1a6cb06bd8d7799bb2deccc2e0d6c67
user <kola89611@gmail.com>
2024-06-05 19:21:12 +0300

    Added: base models for account

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,33 @@
-from django.db import models
+from django.db.models import (
+    Model,
+    ForeignKey,
+    CharField,
+    DateTimeField,
+    EmailField,
+    PROTECT,
+)
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+GENDERS = [
+    ('unknown', 'Unknown'),
+    ('female', 'Female'),
+    ('male', 'Male'),
+]
+
+
+class UserProfile(Model):
+    class Meta:
+        verbose_name = 'User Profile'
+        ordering = ['user']
+
+    user = ForeignKey(User, verbose_name='User', on_delete=PROTECT, related_name='user_profile')
+    name = CharField(max_length=50, verbose_name='Name', blank=True)
+    gender = CharField(max_length=50, verbose_name='Gender', choices=GENDERS, default='unknown')
+    date_of_birth = DateTimeField(verbose_name='Date of Birth')
+    about = CharField(max_length=100, verbose_name='About User', blank=True)
+    address = CharField(max_length=50, verbose_name='Address', blank=True)
+    email = EmailField(verbose_name='E-mail', blank=True,)
+
+    def __str__(self):
+        return str(self.user)
