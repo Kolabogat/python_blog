5676bd1e69b0e2b572d5caa4aee5e181b1df91d9
user <kola89611@gmail.com>
2024-06-10 19:05:37 +0300

    Updated: Admin

--- backend/blog/admin.py
+++ backend/blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from blog.models import Article, ArticleViewers
+from blog.models import Article, ArticleViewers, Categories, Difficulties
 
 admin.site.register(Article)
 admin.site.register(ArticleViewers)
+admin.site.register(Categories)
+admin.site.register(Difficulties)
