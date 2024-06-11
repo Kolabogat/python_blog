1edad5d7de0085adea207c98e4dec2c7f962f5e7
user <kola89611@gmail.com>
2024-06-11 16:57:52 +0300

    Added: image, date, Sidebar

--- frontend/src/BlogDetail.js
+++ frontend/src/BlogDetail.js
@@ -1,25 +1,32 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import Sidebar from "./Sidebar";
 import { useNavigate } from "react-router-dom";
 
 const BlogDetail = () => {
   const { id } = useParams();
   const { data: blog, error } = useFetch('http://localhost:8000/api/blog/' + id + '/');
   const navigate = useNavigate();
+  const { data: categories, cat_error } = useFetch('http://localhost:8000/api/categories/')
 
   return (
-    <div className='blog-details'>
-      {console.log('Data: ' + error)}
-      { error && <div>Error: { error }</div>}
-      { blog && (
-        <div>
-          <h1>{ blog.title }</h1>
-          <span className='span-category'>{ blog.category.category }</span> · <span className='difficulty-easy'>{ blog.difficulty.difficulty }</span><span> · Written by { blog.user } · { blog.words_number } Words</span>
-          { blog.image && <img className='image-field' src={ blog.image } alt="Paris" /> }
+    <div className='container'>
+      <div className='content'>
+        <div className='blog-details'>
+          { error && <div>Error: { error }</div>}
+          { blog && (
+            <div>
+              { blog.image && <img className='image-field' src={ blog.image } alt="Paris" /> }
 
-          <p>{ blog.content }</p>
+              <h1>{ blog.title }</h1>
+              <span className='span-category'>{ blog.category.category }</span> · <span className={ blog.difficulty.class_name }>{ blog.difficulty.difficulty }</span><span> · Written by { blog.user }</span>
+              <p>{ blog.content }</p>
+              <p className='datetime'>Posted { blog.created_at }</p>
+            </div>
+          )}
         </div>
-      )}
+      </div>
+      { categories && <Sidebar categories={ categories.results } title='Categories' /> }
     </div>
   );
 }
