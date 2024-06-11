e5ffffa7a40f2e1c693cf638f8b046b2fc004e89
user <kola89611@gmail.com>
2024-06-11 23:54:36 +0300

    Added: additional fields for ViewPagination

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,4 +1,5 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework import pagination
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.parsers import FileUploadParser, MultiPartParser, FormParser
@@ -11,9 +12,37 @@ from rest_framework.response import Response
 
 
 class ArticleAPIListPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 2
     page_size_query_param = 'page_size'
-    max_page_size = 201
+    max_page_size = 20
+
+    def get_paginated_response(self, data):
+        return Response({
+            'page_size': self.page_size,
+            'total_objects': self.page.paginator.count,
+            'total_pages': self.page.paginator.num_pages,
+            'previous_page_number': self.previous_page_number(),
+            'current_page_number': self.page.number,
+            'next_page_number': self.next_page_number(),
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data,
+        })
+
+    def previous_page_number(self):
+        current_page_number = self.page.number
+
+        if current_page_number > 1:
+            return current_page_number - 1
+        return None
+
+    def next_page_number(self):
+        current_page_number = self.page.number
+        total_pages = self.page.paginator.num_pages
+
+        if total_pages > current_page_number:
+            return current_page_number + 1
+        return None
 
 
 class ArticleAPIList(generics.ListCreateAPIView):
@@ -48,13 +77,13 @@ class ArticleAPIDestroy(generics.RetrieveDestroyAPIView):
 
 class ArticleAPIFilter(generics.ListAPIView):
     serializer_class = ArticleSerializer
+    pagination_class = ArticleAPIListPagination
     # permission_classes = (IsOwnerOrReadOnly, )
 
     def get_queryset(self, *args, **kwargs):
         filter_word = self.kwargs.get('filter')
         category = Categories.objects.get(slug=filter_word)
         return Article.objects.filter(category=category)
-
 
 
 class CategoriesAPIList(generics.ListAPIView):
