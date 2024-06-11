737f336b87b0be6dcbeae1c6b84916c59dd39e96
user <kola89611@gmail.com>
2024-06-11 16:51:37 +0300

    Added: DATETIME_FORMAT

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,6 +136,7 @@ MEDIA_URL = "/media/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DATETIME_FORMAT': "%d.%m.%Y",
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
     'DEFAULT_RENDERER_CLASSES': [
