66d8ccbda42b4920873da89cd54bdb61ac365caf
user <kola89611@gmail.com>
2024-06-11 23:52:25 +0300

    Added: Paginate elements

---
+++ frontend/src/BlogFilterPaginate.js
@@ -0,0 +1,24 @@
+import BlogList from './BlogList';
+import Sidebar from './Sidebar';
+import useFetch from './useFetch';
+import { useParams } from "react-router-dom";
+
+
+const BlogFilterPaginate = () => {
+  const { slug, page } = useParams();
+  const { data: blogs, error } = useFetch('http://localhost:8000/api/blog/filter=' + slug + '/?page=' + page)
+  const { data: categories, cat_error } = useFetch('http://localhost:8000/api/categories/')
+
+  return (
+    <div className="Home">
+      <div className='container'>
+        { blogs && <BlogList blogs={ blogs } title={ `${ slug } blogs` } url={ `/filter_by/${ slug }` } /> }
+        { error }
+        { cat_error }
+        { categories && <Sidebar categories={ categories.results } title='Categories' /> }
+      </div>
+    </div>
+  );
+}
+
+export default BlogFilterPaginate;
---
+++ frontend/src/BlogPaginate.js
@@ -0,0 +1,25 @@
+import BlogList from './BlogList';
+import Sidebar from './Sidebar';
+import useFetch from './useFetch';
+import Paginate from './Paginate';
+import { useParams } from "react-router-dom";
+
+
+const BlogPaginate = () => {
+  const { page } = useParams();
+  const { data: blogs, error } = useFetch('http://localhost:8000/api/blog_list/?page=' + page)
+  const { data: categories, cat_error } = useFetch('http://localhost:8000/api/categories/')
+
+  return (
+    <div className="Home">
+      <div className='container'>
+        { blogs && <BlogList blogs={ blogs } title='All blogs' url='' /> }
+        { error }
+        { cat_error }
+        { categories && <Sidebar categories={ categories.results } title='Categories' /> }
+      </div>
+    </div>
+  );
+}
+
+export default BlogPaginate;
