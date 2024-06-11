8b0e26c60835ebcc13d35348b928784dfde50cdd
user <kola89611@gmail.com>
2024-06-11 16:52:58 +0300

    Updated: Sidebar

--- frontend/src/Sidebar.js
+++ frontend/src/Sidebar.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import useFetch from './useFetch';
 
 const Sidebar = ({categories, title}) => {
@@ -5,13 +6,20 @@ const Sidebar = ({categories, title}) => {
   return (
     <div className='sidebar'>
       <h2>{ title }</h2>
-      <div className='categories'>
-      {categories.map((category) => (
-        <div className='category' key={category.id}>
-            <p>{category.category}</p>
-        </div>
-      ))}
+      <div className='categories '>
+        <Link to='/'>
+          <div className='category'>
+            <p>All</p>
+          </div>
+        </Link>
 
+        {categories.map((category) => (
+          <Link to={ `/filter_by/${ category.slug }` } className="text-decoration: none;">
+            <div className='category' key={category.id}>
+              <p>{category.category}</p>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
 
